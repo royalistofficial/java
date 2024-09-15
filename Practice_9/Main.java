9219fc6661990d89126d82d7d60d3b4ecd36bfea
royalistofficial <mishabygaga1414@gmail.com>
2024-09-15 20:11:53 +0300

    Practice 9

---
+++ Practice_9/Main.java
@@ -0,0 +1,74 @@
+/**
+    * @author Бугайцев Михаил
+    * @version 1.0
+    * @param нет передаваемх параметров
+    * @return нет возвращаемого значения
+    * @exception Нет исключений, генерируемых методом
+    * @throws Нет исключений, генерируемых методом
+    * @see java.lang.Object
+*/
+public class Main {
+
+    /**
+     * Метод main, который запускает практики.
+     * 
+     * @param args нет передаваемых параметров
+     * @return нет возвращаемого значения
+     * @exception Нет исключений, генерируемых методом
+     * @throws Нет исключений, генерируемых методом
+     * @see java.lang.Object
+     */
+    public static void main(String[] args) {
+        practice1();
+        practice2();
+    }
+
+    /**
+     * Практика #1: Примеры использования различных типов переменных в операторе switch.
+     * 
+     * @param нет передаваемых параметров
+     * @return нет возвращаемого значения
+     * @exception Нет исключений, генерируемых методом
+     * @throws Нет исключений, генерируемых методом
+     * @see java.lang.Object
+     */
+    private static void practice1() {
+        int i = 0;
+        while (true) {
+            System.out.println(i);
+            i++;
+        }
+
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        } while (true);
+    }
+
+    /**
+     * Практика #2: Пример использования операторов break и continue с любым видом цикла.
+     * 
+     * @param нет передаваемых параметров
+     * @return нет возвращаемого значения
+     * @exception Нет исключений, генерируемых методом
+     * @throws Нет исключений, генерируемых методом
+     * @see java.lang.Object
+     */
+    private static void practice2() {
+        for (int k = 0; k < 10; k++) {
+            if (k == 5) {
+                System.out.println("break");
+                break;
+            }
+            System.out.println(k);
+        }
+
+        for (int l = 0; l < 10; l++) {
+            if (l % 2 == 0) {
+                continue;
+            }
+            System.out.println(l);
+        }
+    }
+}
