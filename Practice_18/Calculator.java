f8b38a70007f99a7fd69ad2d7535c0dc4468ef29
royalistofficial <mishabygaga1414@gmail.com>
2024-11-01 13:53:30 +0300

    Practice_18

---
+++ Practice_18/Calculator.java
@@ -0,0 +1,34 @@
+package Practice_18;
+
+public class Calculator {
+
+    // Метод для сложения двух чисел
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Перегруженный метод для сложения переменного числа целых чисел
+    public int add(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    // Перегруженный метод для сложения переменного числа чисел типа double
+    public double add(double... numbers) {
+        double sum = 0.0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5, 10));
+        System.out.println(calculator.add(1, 2, 3, 4, 5));
+        System.out.println(calculator.add(1.1, 2.2, 3.3, 4.4));
+    }
+}
