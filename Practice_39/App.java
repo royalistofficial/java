8a972a41bc90217cfcdf67034dd55a16b0d6c7ee
royalistofficial <mishabygaga1414@gmail.com>
2024-11-24 20:19:16 +0300

    Practice_39

---
+++ Practice_39/App.java
@@ -0,0 +1,9 @@
+public class App {
+  final static int START_COUNTER;
+  static {
+    START_COUNTER = Integer.parseInt("Y-");
+  }
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
